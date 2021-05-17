09c50e74244de1656186df2369e07bce27beac4a
Omar Ganiev <beched@deteact.com>
2021-05-17 19:40:42 +0300

    level 5 done

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,13 +9,20 @@ module.exports = {
       forking: {
         url: "https://eth-ropsten.alchemyapi.io/v2/DX39e67S1a4pmCjgGKavlEOvl9RUCNGT",
         blockNumber: 10144911
-      }
+      },
+      allowUnlimitedContractSize: true
     },
   },
   solidity: {
     compilers: [
       {
         version: "0.5.3"
+      },
+      {
+        version: "0.6.5"
+      },
+      {
+        version: "0.6.12"
       },
       {
         version: "0.6.6"
