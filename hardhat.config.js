b60033a9f0e1b29cbe53fe79144cff28d582f982
Raz0r <me@raz0r.name>
2021-05-18 16:34:01 +0300

    increase gas limit for hardhat tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("@nomiclabs/hardhat-truffle5");
 module.exports = {
   networks: {
     hardhat: {
-      blockGasLimit: 8000000,
+      blockGasLimit: 9500000,
       forking: {
         url: "https://eth-ropsten.alchemyapi.io/v2/DX39e67S1a4pmCjgGKavlEOvl9RUCNGT",
         blockNumber: 10144911
